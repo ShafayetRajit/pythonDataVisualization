fd34f75bb890590aa9e17dfb7f0e74a45c7db42c
Shafayet Rajit <sr.000297@gmail.com>
2021-09-03 23:16:23 +0600

    Add files via upload

---
+++ data_plot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../../sample_data/02 Introduction to Pandas/intel.csv', index_col = 'Date', parse_dates = True)
+
+#print(df.head())
+
+
+df['Close'].plot()
+plt.axis(('10-10-17', '24-04-18', 30, 60))
+plt.title('Stock Price')
+plt.show()
+plt.savefig('stockprice.pdf')

de469937987eef75860cc87cf2cf5139449c32bf
Shafayet Rajit <sr.000297@gmail.com>
2021-09-05 11:10:55 +0600

    Update data_plot_scatter_bar.py

--- code/data_plot_scatter_bar.py
+++ code/data_plot_scatter_bar.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 expec = pd.read_csv('../sample_data/irish_life_expec.csv')
 
 expec.plot(kind = 'bar', x = 'year', y = 'life expec', color='g', legend = True)
-expec.plot(kind = 'scatter', x = 'year', y = 'life expec')
+expec.plot(kind = 'scatter', x = 'year', y = 'life expec') #scatter plot
 
 
 

cfc6365acf784d1be4fd2fedcec849e4b0699559
Shafayet Rajit <sr.000297@gmail.com>
2021-09-05 11:06:51 +0600

    Add files via upload

---
+++ code/data_analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+stock = pd.read_csv('../sample_data/03 Data Analysis/tesla.csv') #open file
+
+print(stock.describe()) #prints count, mean, std, 25/50/75%, max
+
+print(stock['Close'].mean()) #mean of a specific column
+print(stock['Close'].min()) #min of a specific column
+print(stock['Close'].max()) #max of a specific column
+
+stock['Close'].plot(kind = 'box') #boxplot of close values 
+plt.show()
---
+++ code/data_filter.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+app_data = pd.read_csv('../sample_data/02 Introduction to Pandas/googleplaystore.csv') #read file
+
+print(app_data)
+
+print(app_data[app_data['Rating'] >= 4.9]) #conditional
+
+print(app_data[(app_data['Genres']=='Education') & (app_data['Rating'] >= 4.9)]) #combining conditions

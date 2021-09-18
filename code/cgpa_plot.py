231d04f58017e5c40da6d422590b26ed27b77aa6
Shafayet Rajit <sr.000297@gmail.com>
2021-09-18 10:14:43 +0600

    Update cgpa_plot.py

--- code/cgpa_plot.py
+++ code/cgpa_plot.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 
 cgpa = pd.read_csv('../sample_data/cgpa.csv', index_col='semester')
 
-cgpa.plot(y=['CGPA','CS CGPA'], style='.-', title='Completely RANDOM CGPA')
+cgpa.plot(y=['CGPA','CS CGPA'], style='.-', title='Completely RANDOM CGPA') #can also be done using .loc, then must provide timeframe
 plt.ylabel('CGPA')
 plt.show()
